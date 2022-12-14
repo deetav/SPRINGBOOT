4a316baa80f6b77912891fb709ab8b5825bf6919
Deepak <deetav001@gmail.com>
2022-12-14 17:59:18 +0530

    added status entity

---
+++ src/main/java/com/deetav/movieBookingSystem/entities/Status.java
@@ -0,0 +1,44 @@
+package com.deetav.movieBookingSystem.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Status {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int statusId;
+    @Column(length = 20,nullable = false,unique = true)
+    private String statusName;
+
+    public Status(){
+
+    }
+    public Status(String statusName){
+        this.statusName = statusName;
+    }
+
+    public int getStatusId() {
+        return statusId;
+    }
+
+    public void setStatusId(int statusId) {
+        this.statusId = statusId;
+    }
+
+    public String getStatusName() {
+        return statusName;
+    }
+
+    public void setStatusName(String statusName) {
+        this.statusName = statusName;
+    }
+
+    @Override
+    public String toString() {
+        return "Status{" +
+                "statusId=" + statusId +
+                ", statusName='" + statusName + '\'' +
+                '}';
+    }
+}
